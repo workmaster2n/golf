58d733d7d1f1b91000bbbccf91c25325cc0faaa0
Tyler DeWitt <tyler.dewitt@colorado.edu>
2014-10-17 14:23:10 -0500

    so many cool things

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,16 @@
 class Course < ActiveRecord::Base
+  has_many :holes
+
+  validates_uniqueness_of :name
+
+  after_create :make_holes
+
+  private
+
+  def make_holes
+    (1..18).each do |number|
+      holes.create(number: number)
+    end
+  end
+
 end
--- app/models/hole.rb
+++ app/models/hole.rb
@@ -1,2 +1,4 @@
 class Hole < ActiveRecord::Base
+  belongs_to :course
+
 end
